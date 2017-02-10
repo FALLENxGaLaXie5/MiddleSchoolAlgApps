f2794f52385cd06c003305cea60ef20da5133f32
FALLENxGaLaXie5 <steward.joshua@rocketmail.com>
2017-02-10 13:58:10 -0800

    init com

---
+++ ProgressInMathApps/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public float positionalValue = 0;
+    public void Slider_Changed(float newValue)
+    {
+        positionalValue = newValue;
+    }
+
+    public void moveTrain()
+    {
+        //move train to the positional value of that track
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ ProgressInMathApps/Assets/Scripts/GenerateTrack.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateTrack : MonoBehaviour
+{
+    // Use this for initialization
+
+    public int numberOfTracks;
+    //int currentTrackGeneration = 1;
+
+    public GameObject trackPrefab;
+    public GameObject startTrack;
+
+
+    GameObject currentTrack;
+    GameObject currentTrackStart;
+    GameObject currentTrackEnd;
+	void Start ()
+    {
+        currentTrack = startTrack.gameObject;
+        currentTrackStart = currentTrack.transform.Find("EndPoint").gameObject;
+        currentTrackEnd = currentTrack.transform.Find("StartPoint").gameObject;
+        generateTrack();
+	}
+
+    void generateTrack()
+    {
+        for(int i = 1; i < numberOfTracks; i++)
+        {
+            currentTrack = Instantiate(trackPrefab, currentTrackEnd.transform.position, Quaternion.identity);
+            currentTrackStart = currentTrack.transform.Find("EndPoint").gameObject;
+            currentTrackEnd = currentTrack.transform.Find("StartPoint").gameObject;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
